6b5a8550f5b56a8df3c7a3bffb815e20b9e80591
Flavio Salirrosas <favio789@gmail.com>
2020-12-22 17:41:22 -0500

    cambio de conexion

--- Breza/modelo/conexion.php
+++ Breza/modelo/conexion.php
@@ -2,7 +2,7 @@
 define("HOST","localhost");
 define("USER","root");
 define("PASSWORD","12345678");
-define("BD","breza"); 
+define("BD","sistemabreza"); 
 
 class Conexion{
 	public static $conexion = null;
