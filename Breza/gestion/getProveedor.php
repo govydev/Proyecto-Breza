8a86e8e1e3be40a235c6fed783e4717d43da798d
pekopeko <rodrigoevret@gmail.com>
2021-01-11 15:37:31 -0500

    Se créo el CRUD de proveedores

---
+++ Breza/gestion/getProveedor.php
@@ -0,0 +1,56 @@
+<?php
+include_once("../modelo/proveedor.php");
+include_once("neoProveedor.php");
+include_once("proveedor.php");
+header('Cache-Control: no cache'); 
+session_cache_limiter('private_no_expire');
+
+if(isset($_POST['accion'])){
+
+}else{
+    $_POST['accion'] = "";
+}
+
+session_start();
+if ($_SESSION["acceso"]) {
+    switch ($_POST['accion']){   
+        case 'Nuevo':
+            $formProveedor = new formNP;
+            $formProveedor->formNPShow("NUEVA");
+            break;
+        case 'Editar':
+            $objProveedor = new proveedor;
+            $formProveedor = new formNP;
+            $proveedor = $objProveedor -> buscar($_POST["id"]);
+            $formProveedor->formNPShow("MODIFICAR", $proveedor);
+            break;
+        case 'Guardar':
+            $objProveedor = new proveedor;
+            switch ($_POST["registrar"]){
+                case 'NUEVA':
+                    $datos = [trim($_POST["txtNombre"]),trim($_POST["txtRuc"]),trim($_POST["txtTelefono"]),
+                    trim($_POST["txtCorreo"]),trim($_POST["txtDireccion"]),trim($_POST["txtEstado"])];
+                    $objProveedor->agregar($datos);
+                    header('Location: getProveedor.php');
+                    break;
+                case 'MODIFICAR':
+                    $id = trim($_POST["txtId"]);
+                    $datos = [trim($_POST["txtNombre"]),trim($_POST["txtRuc"]),trim($_POST["txtTelefono"]),
+                    trim($_POST["txtCorreo"]),trim($_POST["txtDireccion"]),trim($_POST["txtEstado"])];
+                    $objProveedor->editar($id, $datos);
+                    header('Location: getProveedor.php');
+                    break;
+                default:
+                    header('Location: getProveedor.php');
+                    break;
+            }
+        default:
+            $formProveedor = new formProveedor;
+            $objProveedor = new proveedor;
+            $lista = $objProveedor -> listar();
+            $formProveedor->formProveedorShow($_SESSION["privilegios"], $lista);
+            break;
+    }
+}else{
+    header("Location: ../index.php");   
+}
--- Breza/modelo/proveedor.php
+++ Breza/modelo/proveedor.php
@@ -1,36 +1,36 @@
 <?php
 include_once("conexion.php");
 
-class Proveedor{
+class proveedor{
 
-    public function listaProveedores(){
+    public function listar(){
         $lista = Conexion::select("SELECT * FROM proveedor");
         return $lista;
     }
 
-    public function ProveedorId($id){
-        $maquina = Conexion::select("SELECT * FROM proveedor WHERE idproveedor = $id");
+    public function buscar($id){
+        $proveedor = Conexion::select("SELECT * FROM proveedor WHERE idproveedor = $id");
         return $proveedor[0]; 
     }
 
-    public function modificar($id, $nombre, $ruc, $telefono, $correo, $direccion, $estado){
-        $maquina = Conexion::query("UPDATE proveedor SET Nombre = '$nombre', RUC = '$ruc', Telefono = '$telefono', Correo = '$correo', Direccion = $direccion, Estado = '$estado', WHERE idproveedor = $id");
+    public function editar($id, $datos){
+        $proveedor = Conexion::query("UPDATE proveedor SET Nombre = '$datos[0]', RUC = '$datos[1]', Telefono = '$datos[2]', Correo = '$datos[3]', Direccion = '$datos[4]', Estado = '$datos[5]' WHERE idproveedor = $id");
         return $proveedor;
     }
 
     public function cambioEstado($id, $estado){
-        $maquina = Conexion::query("UPDATE proveedor SET Estado = $estado WHERE idproveedor = $id");
+        $proveedor = Conexion::query("UPDATE proveedor SET Estado = $estado WHERE idproveedor = $id");
         return $proveedor;
     }
 
     public function eliminar($id){
-        $maquina = Conexion::query("DELETE FROM proveedor WHERE idproveedor = $id");
+        $proveedor = Conexion::query("DELETE FROM proveedor WHERE idproveedor = $id");
         return $proveedor;
     }
 
-    public function agregar($nombre, $ruc, $telefono, $correo, $direccion, $estado){
-        $maquina = Conexion::query("INSERT INTO mantemiento(Nombre, RUC, Telefono, Correo, Direccion, Estado) 
-                                VALUES ('$nombre', '$ruc', '$telefono', '$correo', '$direccion', '$estado')");
+    public function agregar($datos){
+        $proveedor = Conexion::query("INSERT INTO proveedor(Nombre, RUC, Telefono, Correo, Direccion, Estado) 
+                VALUES ('$datos[0]', '$datos[1]', '$datos[2]', '$datos[3]', '$datos[4]', '$datos[5]')");
         return $proveedor;
     }   
 }
