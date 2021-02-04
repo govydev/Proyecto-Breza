f886f804b1218c8314049b23dd599703b9758c57
Flavio Salirrosas <favio789@gmail.com>
2021-02-04 16:54:14 -0500

    conexion provicional

---
+++ Breza/modelo/conexion.php
@@ -0,0 +1,33 @@
+<?php
+define("HOST","localhost");
+define("USER","root");
+define("PASSWORD","");
+define("BD","breza");
+
+class Conexion{
+	public static $conexion = null;
+
+	private function __construct(){}
+
+	public static function select($consulta){
+		if(self::$conexion == null){
+			self::$conexion = new mysqli(HOST, USER,PASSWORD, BD) or die(mysql_error());
+			self::$conexion->set_charset('utf8');
+		}
+		$list = mysqli_query(self::$conexion, $consulta)->fetch_all();
+		return $list;
+	}
+
+	public static function query($consulta, $id = null){
+		if(self::$conexion == null){
+			self::$conexion= new mysqli(HOST, USER,PASSWORD, BD) or die(mysql_error());
+			self::$conexion->set_charset('utf8');
+		}
+		$res = mysqli_query(self::$conexion, $consulta).mysqli_error(self::$conexion);
+		return $id == null? $res: self::$conexion->insert_id;
+	}
+    
+}
+
+?>
+
