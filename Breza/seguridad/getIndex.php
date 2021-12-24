4f6e67870135e210293c7148da4c387addf21258
pekopeko <rodrigoevret@gmail.com>
2021-12-23 20:58:18 -0500

    Creacion de facadeLogin.php

--- Breza/seguridad/getIndex.php
+++ Breza/seguridad/getIndex.php
@@ -1,29 +1,13 @@
 <?php
-include_once("../modelo/usuario.php");
-include_once("../modelo/privilegios.php");
-include_once("autenticacion.php");
-include_once("principal.php");
+include_once("facadeLogin.php");
 header('Cache-Control: no cache'); 
 session_cache_limiter('private_no_expire');
 
 if(isset($_POST['login'])){
     $user = $_POST["txtUser"];
     $password = $_POST["txtPassword"];
-    $usuario =  new Usuario;
-    $respuesta = $usuario->autenticacion(trim($user), trim($password));
-    if($respuesta[0] > 0){
-        $carga = TRUE;
-        $privilegios = new Privilegios();
-        $path = $privilegios->privilegiosUsuario($respuesta[0]);
-        session_start();
-        $_SESSION['user'] = $respuesta;
-        $_SESSION['acceso'] = true; 
-        $_SESSION['privilegios'] = $path;
-        $principal = new Principal();
-        $principal->formPrincipal($path);
-    }else{
-        header("Location: ../index.php?msg=$respuesta");
-    }
+    $facadeLogin = new FacadeLogin;
+    $facadeLogin->autenticacion($user, $password);
 }else{
     session_start();
     session_destroy();
